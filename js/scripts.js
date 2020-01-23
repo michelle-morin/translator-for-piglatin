f5fa9ed1376f43fb50d6e43f433913efdbc0d332
Abe Ybarra and Michelle Morin <abe@>
2020-01-23 10:33:14 -0800

    attempt to separate backend and frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
-//Back-end logic:
+// Back-end logic:
 
+function pigLatin(str) {
+  var array = str.split("");
+  array.forEach(function(element) {
+    if (isNaN(element) === false) {
+      element = element;
+    } else if (array.length === 1 && (array[0] === "a" || array[0] === "i" || array[0] === "A" || array[0] === "I")) {
+      array.push("ay");
+    }
+  })
+}
 
-
-
-
-
-//Front-end logic:
+// Front-end logic:
 
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
-    var sentence = $("input#sentence").val();
-    var array = sentence.split("");
-    array.forEach(function (element) {
-      if (isNaN(element) === false) {
-        element = element;
-      }
-    });
-    $("#output").text(array).show();
-  })
+    var sentenceInput = $("input#sentence").val();
+
+    pigLatin(sentenceInput);
+    var newSentence = array.join("");
+    
+    // array.forEach(function (element) {
+    //   if (isNaN(element) === false) {
+    //     element = element;
+    //   } else if (sentence === "a" || sentence === "i" || sentence === "A" || sentence === "I") {
+    //     sentence = sentence + "ay";
+    //   } 
+    // });
+    $("#output").text(newSentence).show();
+  });
 });
