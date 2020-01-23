ddc1cfcc70cf5a381afd7240a05a1f623438a175
Abe Ybarra and Michelle Morin <abe@>
2020-01-23 15:23:59 -0800

    add backend logic for moving multiple consonants at the beginning of a word and adding ay to the end of the word

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,40 @@
 function pigLatin(str) {
   var array = str.split("");
   var words = str.split(" ");
-  var consonants = ["q", "w", "r", "t", "y", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+  var consonants = ["q", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
   var vowels = ["a", "i", "e", "o", "u"];
+
   if (array.length === 1 && (array[0] === "a" || array[0] === "i")) {
     array.push("ay");
     return array;
-  } else if (array.length === 1 && isNaN(parseInt(str)) === false) {
+  } else if (words.length === 1 && isNaN(parseInt(str)) === false) {
     return array;
-  } else if (array.length > 1) {
-    for (index = 0; index < array.length; index += 1) {
-      if (isNaN(array[index]) === false) {
-        array[index] = array[index];
-      } else if (words.length === 1 && (vowels.includes(array[0]))) {
-        array.push("ay");
-      } else if (words.length === 1 && (consonants.includes(array[0]))) {
-        var firstLetter = array.shift();
-        console.log(firstLetter);
-        array.push(firstLetter);
-        console.log(array);
-        array.push("ay");
-        console.log(array);
-      } 
-     }
-    return array;
+  } else if (words.length === 1 && vowels.includes(array[0])) {
+    array.push("ay");
+  } else if (words.length === 1 && consonants.includes(array[0])) {
+    var cloneArray = array.slice();
+    for (index = 0; consonants.includes(array[index]); index += 1) {
+      var firstLetter = cloneArray.shift();
+      cloneArray.push(firstLetter);
     }
-}
+    cloneArray.push("ay");
+    return cloneArray;
+  } else if (words.length === 1 && array[0] === "y") {
+    var cloneArray = array.slice();
+    var firstLetter = cloneArray.shift();
+    cloneArray.push(firstLetter);
+    console.log(cloneArray);
+    for (index = 1; consonants.includes(array[index]); index += 1) {
+      var nextLetter = cloneArray.shift();
+      console.log(nextLetter);
+      cloneArray.push(nextLetter);
+      console.log(cloneArray);
+    }
+    cloneArray.push("ay");
+    return cloneArray;
+  }
+}   
+
 
 
 // Front-end logic:
