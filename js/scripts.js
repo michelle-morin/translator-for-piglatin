2006cd271422271ceb5956d53497bc6fc9adb7f8
Michelle Morin <morin2m@gmail.com>
2020-01-23 20:07:24 -0800

    refactor logic to translate multi-word inputs into pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,70 @@
 // Back-end logic:
 
 function pigLatin(str) {
-  var array = str.split("");
-  var words = str.split(" ");
+  var wordArray = str.split("");
   var consonants = ["q", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
   var vowels = ["a", "i", "e", "o", "u"];
 
-  if (words.length === 1) {
-    if (array.length === 1 && (array[0] === "a" || array[0] === "i")) {
-    array.push("ay");
-    return array;
-  } else if (isNaN(parseInt(str)) === false) {
-    return array;
-  } else if (vowels.includes(array[0])) {
-    array.push("ay");
-    return array;
-  } else if (consonants.includes(array[0]) && str.includes("qu")) {
-    var cloneArray = array.slice();
-    for (var index = 0; consonants.includes(array[index]); index += 1) {
+  if (wordArray.length === 1 && (wordArray[0] === "a" || wordArray[0] === "i")) {
+  wordArray.push("ay");
+  var outputWord = wordArray.join("");
+  return outputWord;
+} else if (isNaN(parseInt(str)) === false) {
+  var outputWord = wordArray.join("");
+  return outputWord;
+} else if (vowels.includes(wordArray[0])) {
+  wordArray.push("ay");
+  var outputWord = wordArray.join("");
+  return outputWord;
+} else if (consonants.includes(wordArray[0]) && str.includes("qu")) {
+  var cloneArray = wordArray.slice();
+  for (var index = 0; consonants.includes(wordArray[index]); index += 1) {
+    var firstLetter = cloneArray.shift();
+    cloneArray.push(firstLetter);
+  } 
+  var letterU = cloneArray.shift();
+  cloneArray.push(letterU);
+  cloneArray.push("ay");
+  var outputWord = cloneArray.join("");
+  return outputWord;
+  } else if (consonants.includes(wordArray[0])) {
+    var cloneArray = wordArray.slice();
+    for (var index = 0; consonants.includes(wordArray[index]); index += 1) {
       var firstLetter = cloneArray.shift();
       cloneArray.push(firstLetter);
-    } 
-    var letterU = cloneArray.shift();
-    cloneArray.push(letterU);
-    cloneArray.push("ay");
-    return cloneArray;
-    } else if (consonants.includes(array[0])) {
-      var cloneArray = array.slice();
-      for (var index = 0; consonants.includes(array[index]); index += 1) {
-        var firstLetter = cloneArray.shift();
-        cloneArray.push(firstLetter);
-       }
-    cloneArray.push("ay");
-    return cloneArray;
-    } else if (array[0] === "y") {
-      var cloneArray = array.slice();
-      var firstLetter = cloneArray.shift();
-      cloneArray.push(firstLetter);
-      for (var index = 1; consonants.includes(array[index]); index += 1) {
-        var nextLetter = cloneArray.shift();
-        cloneArray.push(nextLetter);
       }
-      cloneArray.push("ay");
-      return cloneArray;
+  cloneArray.push("ay");
+  var outputWord = cloneArray.join("");
+  return outputWord;
+  } else if (wordArray[0] === "y") {
+    var cloneArray = wordArray.slice();
+    var firstLetter = cloneArray.shift();
+    cloneArray.push(firstLetter);
+    for (var index = 1; consonants.includes(wordArray[index]); index += 1) {
+      var nextLetter = cloneArray.shift();
+      cloneArray.push(nextLetter);
     }
-  } else if (words.length > 1) {
-    alert("please only enter one word; my logic is broken!");
+    cloneArray.push("ay");
+    var outputWord = cloneArray.join("");
+    return outputWord;
   }
-}
+} 
 
 // Front-end logic:
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var sentenceInput = $("input#sentence").val().toLowerCase();
+    var sentenceInput = $("input#sentence").val().toLowerCase().split(" ");
 
-    var newArray = pigLatin(sentenceInput);
+    var pigLatinArray = [];
+    sentenceInput.forEach(function(word) {
+      var pigLatinWord = pigLatin(word);
+      pigLatinArray.push(pigLatinWord);
+    })
 
-    var newSentence = newArray.join("").toLowerCase();;
+    var pigLatinSentence = pigLatinArray.join(" ").toLowerCase();;
     
-    $("#output").text(newSentence).show();
+    $("#output").text(pigLatinSentence).show();
   });
 });
