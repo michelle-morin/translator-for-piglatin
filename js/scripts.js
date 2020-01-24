daa631cde02f3a416a0d5ccd76d0ced40c0c7444
Abe Ybarra and Michelle Morin <abe@>
2020-01-23 16:19:24 -0800

    add return to logic for multicharacter words beginning with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -6,43 +6,46 @@ function pigLatin(str) {
   var consonants = ["q", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
   var vowels = ["a", "i", "e", "o", "u"];
 
-  if (array.length === 1 && (array[0] === "a" || array[0] === "i")) {
+  if (words.length === 1) {
+    if (array.length === 1 && (array[0] === "a" || array[0] === "i")) {
     array.push("ay");
     return array;
-  } else if (words.length === 1 && isNaN(parseInt(str)) === false) {
+  } else if (isNaN(parseInt(str)) === false) {
     return array;
-  } else if (words.length === 1 && vowels.includes(array[0])) {
+  } else if (vowels.includes(array[0])) {
     array.push("ay");
-  } else if (words.length === 1 && consonants.includes(array[0]) && str.includes("qu")) {
+    return array;
+  } else if (consonants.includes(array[0]) && str.includes("qu")) {
     var cloneArray = array.slice();
     for (var index = 0; consonants.includes(array[index]); index += 1) {
       var firstLetter = cloneArray.shift();
       cloneArray.push(firstLetter);
     } 
-      var letterU = cloneArray.shift();
-      cloneArray.push(letterU);
-      cloneArray.push("ay");
-      return cloneArray;
-    } else if (words.length === 1 && consonants.includes(array[0])) {
-    var cloneArray = array.slice();
-    for (var index = 0; consonants.includes(array[index]); index += 1) {
+    var letterU = cloneArray.shift();
+    cloneArray.push(letterU);
+    cloneArray.push("ay");
+    return cloneArray;
+    } else if (consonants.includes(array[0])) {
+      var cloneArray = array.slice();
+      for (var index = 0; consonants.includes(array[index]); index += 1) {
+        var firstLetter = cloneArray.shift();
+        cloneArray.push(firstLetter);
+       }
+    cloneArray.push("ay");
+    return cloneArray;
+    } else if (array[0] === "y") {
+      var cloneArray = array.slice();
       var firstLetter = cloneArray.shift();
       cloneArray.push(firstLetter);
+      for (var index = 1; consonants.includes(array[index]); index += 1) {
+        var nextLetter = cloneArray.shift();
+        cloneArray.push(nextLetter);
+      }
+      cloneArray.push("ay");
+      return cloneArray;
     }
-    cloneArray.push("ay");
-    return cloneArray;
-  } else if (words.length === 1 && array[0] === "y") {
-    var cloneArray = array.slice();
-    var firstLetter = cloneArray.shift();
-    cloneArray.push(firstLetter);
-    for (var index = 1; consonants.includes(array[index]); index += 1) {
-      var nextLetter = cloneArray.shift();
-      cloneArray.push(nextLetter);
-    }
-    cloneArray.push("ay");
-    return cloneArray;
-  }
-}   
+  } 
+}
 
 // Front-end logic:
 
