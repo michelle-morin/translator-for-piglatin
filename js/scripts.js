645eeae7a2dfb83f2026117e472c0f2a2f234e65
Michelle Morin <morin2m@gmail.com>
2020-01-23 20:24:46 -0800

    change span classes to IDs and update respective references in scripts and styles

--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
     var pigLatinSentence = pigLatinArray.join(" ").toLowerCase();;
     
     $("#output").show();
-    $(".piglatin").text(pigLatinSentence);
-    $(".phrase").text(sentenceInput.join(" "));
+    $("#piglatin").text(pigLatinSentence);
+    $("#phrase").text(sentenceInput.join(" "));
   });
 });
